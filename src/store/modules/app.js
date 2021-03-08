263596a92d7236de44c7a13e6c9e7cbd943f24a5
Vadim <vadim.a.shesterikov@gmail.com>
2021-03-08 02:33:57 +0200

    init store for app module

---
+++ src/store/modules/app.js
@@ -0,0 +1,22 @@
+// initial state
+const state = () => ({
+    drawer: null,
+    items: [],
+})
+
+// getters
+const getters = {}
+
+// actions
+const actions = {}
+
+// mutations
+const mutations = {}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+}

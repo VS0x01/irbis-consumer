d88d4e3507ac6391bf2d27fe544300c6d3f32c28
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-06-04 15:24:22 +0300

    AuthorCard: update skeleton loader

--- src/components/author/AuthorCard.vue
+++ src/components/author/AuthorCard.vue
@@ -1,6 +1,21 @@
 <template>
   <v-card :loading="!author">
-    <v-skeleton-loader type="article" v-if="!author" />
+    <v-container v-if="!author">
+      <v-row>
+        <v-col>
+          <v-skeleton-loader type="card-heading, text" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-skeleton-loader type="card-heading" />
+        </v-col>
+        <v-col class="flex-grow-0" v-for="n in 4" :key="n">
+          <v-skeleton-loader type="chip" />
+        </v-col>
+        <v-spacer />
+      </v-row>
+    </v-container>
     <v-card-title v-if="author">
       {{ author.lastName + ' ' + author.firstName }}
     </v-card-title>
